f6f0a993b006fe5fe5e425bdb1ed59a5fc217a11
Italo Santos Nogueira <00001122442191sp@aluno.educacao.sp.gov.br>
2024-05-22 17:34:12 -0300

    first commit

---
+++ Exercício 3.py
@@ -0,0 +1,8 @@
+quantidade_desejada = int(input("Digite a quantidade desejada: "))
+if quantidade_desejada >=10:
+    print("produto disponivel em estoque. Aproveite!")
+else:
+    print("produto indisponivel no momento. Volte mais tarde!")
+
+
+
